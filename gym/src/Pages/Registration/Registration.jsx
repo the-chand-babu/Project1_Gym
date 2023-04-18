8682d1e8f03f770e13b01523b9eafb0a08539c6b
Dilip <dilip.884400@gmail.com>
2023-04-18 14:05:05 +0530

    changes

--- gym/src/Pages/Registration/Registration.jsx
+++ gym/src/Pages/Registration/Registration.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './Registration.module.css';
 
 const Register = () => {
     return (
         <div>
             <h1>Register</h1>
-            <form>
+            <form className={styles.form}>
+                <label htmlFor="username"> UserName:</label>
+                <input type="username" id="username" name="username" required/>
                 <label htmlFor="email">Email:</label>
                 <input type="email" id="email" name="email" required />
                 <label htmlFor="password">Password:</label>
                 <input type="password" id="password" name="password" required />
                 <label htmlFor="confirmPassword">Confirm Password:</label>
                 <input type="password" id="confirmPassword" name="confirmPassword" required />
-                <button type="submit">Register</button>
+                <button type="submit" className={styles.Register}>Register</button>
             </form>
             <h3>Already have an account?  <Link to ='/login'>Login</Link></h3>
 
@@ -21,3 +24,5 @@ const Register = () => {
 };
 
 export default Register;
+
+
