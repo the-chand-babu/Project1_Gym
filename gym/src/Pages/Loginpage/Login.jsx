39c2fde80e7141949e81fcfab0cf25f0962e2f9a
bharti zade <bhartigzade@gmail.com>
2023-04-18 13:17:52 +0530

    adding css in login and registration

--- gym/src/Pages/Loginpage/Login.jsx
+++ gym/src/Pages/Loginpage/Login.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import './Login.css';
-import { Link } from 'react-router-dom';
+// import React from 'react';
+// import './Login.css';
+// import { Link } from 'react-router-dom';
 
+
+// const Login = () => {
+//     return (
+//         <div className="login-container">
+
+
+
+//             <h1>Login</h1>
+//             <form>
+//                 <label htmlFor="email">Email:</label>
+//                 <input type="email" id="email" name="email" required />
+//                 <label htmlFor="password">Password:</label>
+//                 <input type="password" id="password" name="password" required />
+//                 <button type="submit">Login</button>
+//             </form>
+//             <h3>Don't have an account?  <Link to ='/register'>Register</Link></h3>
+//         </div>
+//     );
+// };
+
+// export default Login;
+import React from 'react';
+import styles from './Login.module.css'; // Import CSS module
+import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className="login-container">
-
-
-
+        <div className={styles.loginContainer}> {/* Apply CSS module class to container */}
             <h1>Login</h1>
             <form>
                 <label htmlFor="email">Email:</label>
                 <input type="email" id="email" name="email" required />
                 <label htmlFor="password">Password:</label>
                 <input type="password" id="password" name="password" required />
-                <button type="submit">Login</button>
+                <button type="submit" className={styles.loginButton}>Login</button> {/* Apply CSS module class to button */}
             </form>
-            <h3>Don't have an account?  <Link to ='/register'>Register</Link></h3>
+            <h3>Don't have an account? <Link to='/register'>Register</Link></h3>
         </div>
     );
 };
