bfbbdc822357190d1c576c89ececa756442b3eab
chand babu <jafri.nbj@gmail.com>
2023-04-18 15:20:11 +0530

    validadation using

--- gym/src/Pages/Registration/Registration.jsx
+++ gym/src/Pages/Registration/Registration.jsx
@@ -1,23 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import styles from './Registration.module.css';
 
 const Register = () => {
+    const initialValues = {
+        username: "",
+        email: "",
+        password: "",
+        confirmPassword: ""
+    }
+
+    const [formValues, setFormValues] = useState(initialValues);
+    const [error, setError] = useState({})
+
+
+    const handelChange = (e) => {
+        const { name, value } = e.target;
+        setFormValues({
+            ...formValues, [name]: value
+        })
+    }
+
+    const handelSubmit = (e) => {
+        e.preventDefault()
+
+       setError(Validation(formValues)) 
+    }
+
+
+    const Validation = (values) => {
+            const errors = {};
+            
+
+    }
+
+
     return (
         <div>
             <h1>Register</h1>
-            <form className={styles.form}>
+            <form className={styles.form} onSubmit={handelSubmit}>
                 <label htmlFor="username"> UserName:</label>
-                <input type="username" id="username" name="username" required/>
+                <input onChange={handelChange} value={formValues.username} type="text" id="username" name="username" required />
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required />
+                <input onChange={handelChange} value={formValues.email} type="text" id="email" name="email" required />
                 <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" required />
+                <input onChange={handelChange} value={formValues.password} type="password" id="password" name="password" required />
                 <label htmlFor="confirmPassword">Confirm Password:</label>
-                <input type="password" id="confirmPassword" name="confirmPassword" required />
+                <input onChange={handelChange} value={formValues.ConfirmPassword} type="password" id="confirmPassword" name="confirmPassword" required />
                 <button type="submit" className={styles.Register}>Register</button>
             </form>
-            <h3>Already have an account?  <Link to ='/login'>Login</Link></h3>
+            <h3>Already have an account?  <Link to='/login'>Login</Link></h3>
 
         </div>
     );
