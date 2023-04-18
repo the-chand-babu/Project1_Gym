747078309cf186505675c8123fd880a350551fed
chand babu <jafri.nbj@gmail.com>
2023-04-18 22:00:05 +0530

    authentication validation

--- gym/src/Pages/Loginpage/Login.jsx
+++ gym/src/Pages/Loginpage/Login.jsx
@@ -1,41 +1,37 @@
-// import React from 'react';
-// import './Login.css';
-// import { Link } from 'react-router-dom';
 
-
-// const Login = () => {
-//     return (
-//         <div className="login-container">
-
-
-
-//             <h1>Login</h1>
-//             <form>
-//                 <label htmlFor="email">Email:</label>
-//                 <input type="email" id="email" name="email" required />
-//                 <label htmlFor="password">Password:</label>
-//                 <input type="password" id="password" name="password" required />
-//                 <button type="submit">Login</button>
-//             </form>
-//             <h3>Don't have an account?  <Link to ='/register'>Register</Link></h3>
-//         </div>
-//     );
-// };
-
-// export default Login;
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './Login.module.css'; // Import CSS module
 import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const obj = {
+        email: "",
+        password: ""
+    };
+
+    const formRef = useRef();
+
+    const handelChange = (e) => {
+
+        const { name, value } = e.target;
+        obj[name] = value;
+        formRef.current = obj;
+    }
+
+    // const formSubmit=(e.pre)=>{
+
+    // }
+    // onSubmit = { formSubmit }
+
     return (
         <div className={styles.loginContainer}> {/* Apply CSS module class to container */}
             <h1>Login</h1>
-            <form>
+            <form >
                 <label htmlFor="email">Email:</label>
-                <input type="email" id="email" name="email" required />
+                <input onChange={handelChange} type="email" id="email" name="email" required />
                 <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" required />
+                <input onChange={handelChange} type="password" id="password" name="password" required />
                 <button type="submit" className={styles.loginButton}>Login</button> {/* Apply CSS module class to button */}
             </form>
             <h3>Don't have an account? <Link to='/register'>Register</Link></h3>
--- gym/src/Pages/Registration/Registration.jsx
+++ gym/src/Pages/Registration/Registration.jsx
@@ -11,7 +11,10 @@ const Register = () => {
     }
 
     const [formValues, setFormValues] = useState(initialValues);
-    const [error, setError] = useState({})
+    const [error, setError] = useState({});
+    const [userdata, setUserdata] = useState(JSON.parse(localStorage.getItem("userdata")) || []);
+    const [isuserPresent, setIsuserPresent] = useState(false);
+    const [isregister, setIsregister] = useState(false);
 
 
     const handelChange = (e) => {
@@ -23,30 +26,132 @@ const Register = () => {
 
     const handelSubmit = (e) => {
         e.preventDefault()
+        setIsuserPresent(false);
+        setIsregister(false)
 
-       setError(Validation(formValues)) 
+        const error = (Validation(formValues));
+        if (Object.keys(error).length == 0) {
+            const result = userFind(formValues.email);
+
+            if (result) {
+                setIsuserPresent(true);
+
+                return;
+            }
+
+
+
+
+
+            setUserdata(
+                [
+                    ...userdata,
+                    formValues
+                ]
+            )
+            localStorage.setItem('userdata', JSON.stringify([
+                ...userdata, formValues
+            ]))
+            setIsregister(true);
+
+
+
+
+            setFormValues({
+                username: "",
+                email: "",
+                password: "",
+                confirmPassword: ""
+
+            });
+
+            setError({})
+
+        } else {
+            setError(error)
+        }
+
     }
 
 
-    const Validation = (values) => {
-            const errors = {};
-            
 
+
+
+
+    const Validation = (values) => {
+        const errors = {};
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/i;
+
+        if (!values.username) {
+            errors.username = "username is required !";
+        }
+
+        if (!values.email) {
+            errors.email = "email is required !";
+        } else if (!regex.test(values.email)) {
+            errors.email = 'this is not valid email';
+        }
+
+        if (!values.password) {
+            errors.password = "Password is required !";
+        } else if (values.password.length < 6) {
+            errors.password = "Password should be greater than 6 character";
+        } else if (values.password.length > 16) {
+            errors.password = "Password should be less than 16 character";
+        } else if (values.password.includes(123) || values.password.includes(1234) || values.password.includes(123456) || values.password.includes(12345678)) {
+            errors.password = 'Password should be Unique'
+        }
+
+        if (!values.confirmPassword) {
+            errors.confirmPassword = "confirm Password is required";
+        } else if (values.password !== values.confirmPassword) {
+            errors.confirmPassword = "Password does'nt match"
+        }
+
+
+        return errors;
+
+    }
+
+
+    const userFind = (email) => {
+        const data = userdata;
+
+        let result = false;
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].email == email) {
+                result = true;
+                break;
+            }
+        }
+        return result;
     }
 
 
     return (
         <div>
+            <h1>{
+                isuserPresent ? "User Already exits please login" : ""
+            }</h1>
+            <h2>{isregister ? "Registration succesfully" : ""}</h2>
             <h1>Register</h1>
             <form className={styles.form} onSubmit={handelSubmit}>
                 <label htmlFor="username"> UserName:</label>
-                <input onChange={handelChange} value={formValues.username} type="text" id="username" name="username" required />
+                <input onChange={handelChange} value={formValues.username} type="text" id="username" name="username" />
+
+                <p className={styles.errorMessage}>{error.username}</p>
                 <label htmlFor="email">Email:</label>
-                <input onChange={handelChange} value={formValues.email} type="text" id="email" name="email" required />
+                <input onChange={handelChange} value={formValues.email} type="text" id="email" name="email" />
+                <p className={styles.errorMessage}>{error.email}</p>
+
                 <label htmlFor="password">Password:</label>
-                <input onChange={handelChange} value={formValues.password} type="password" id="password" name="password" required />
+                <input onChange={handelChange} value={formValues.password} type="password" id="password" name="password" />
+                <p className={styles.errorMessage}>{error.password}</p>
+
                 <label htmlFor="confirmPassword">Confirm Password:</label>
-                <input onChange={handelChange} value={formValues.ConfirmPassword} type="password" id="confirmPassword" name="confirmPassword" required />
+                <input onChange={handelChange} value={formValues.confirmPassword} type="password" id="confirmPassword" name="confirmPassword" />
+                <p className={styles.errorMessage}>{error.confirmPassword}</p>
+
                 <button type="submit" className={styles.Register}>Register</button>
             </form>
             <h3>Already have an account?  <Link to='/login'>Login</Link></h3>
